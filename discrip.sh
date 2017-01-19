8b8742a5510d4c671504ab68c6df4575da7eb144
William Sutton <will@sutton-family.org>
2017-01-19 15:08:55 -0500

    Update discrip.sh

--- discrip.sh
+++ discrip.sh
@@ -9,7 +9,7 @@ cd $WORKINGDIR$RIPPEDDIR
 abcde -j 4 -o flac -p -a cddb,replaygain,tag,getalbumart,clean,cue -D 2>log
 
 #Tell you System is ready for next disc
-curl http://textbelt.com/text -d number=8139921311 -d "message=I am server, insert disc"
+curl http://textbelt.com/text -d number=8138675309 -d "message=I am server, insert disc"
 
 #Insert data into SQL table
 python $WORKINGDIR/disclist.py
